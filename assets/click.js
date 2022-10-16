6be8065ef515d7edd0a1f25662c66eb12e9bf1fe
ZDC7096 <108491910+ZDC7096@users.noreply.github.com>
2022-10-16 12:48:40 -0700

    function change

--- assets/click.js
+++ assets/click.js
@@ -1,4 +1,7 @@
-const btnD = document.getElementById('btnB');
-        function clicked (){
-            document.getElementById("btnB").innerHTML = "CORRECT";
+function change (){
+        let yes = document.getElementById('next').style.color = 'black';
+        document.getElementById("btnB").innerHTML = "CORRECT";
+        document.getElementById('next').innerHTML = 'CLICK FOR NEXT QUESTION!';
+        document.getElementById('next').style.backgroundColor = '#e91700'
     }
+
